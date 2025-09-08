507dca53e63da8ee8e5fc00b6256e1c0f1088531
Mohsin Khan <mohsinsmannat@gmail.com>
2025-09-08 20:24:43 +0530

    added a usetheme to toggle Light/Dark mode

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,18 @@
+import useTheme from "@/hooks/useTheme";
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 const TabsLayout = () => {
+  const { colors } = useTheme();
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
-        tabBarActiveTintColor: "green",
-        tabBarInactiveTintColor: "gray",
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: colors.textMuted,
         tabBarStyle: {
-          backgroundColor: "#1e293b",
+          backgroundColor: colors.surface,
           borderTopWidth: 1,
-          borderTopColor: "yellow",
+          borderTopColor: colors.border,
           height: 90,
           paddingBottom: 30,
           paddingTop: 10,
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
+import useTheme from "@/hooks/useTheme";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const { toggleDarkMode } = useTheme();
   return (
     <View style={styles.container}>
       <Text style={styles.content}>Home Screen.</Text>
+      <TouchableOpacity
+        onPress={() => {
+          toggleDarkMode();
+        }}
+      >
+        <Text>toggle the mode</Text>
+      </TouchableOpacity>
     </View>
   );
 }
